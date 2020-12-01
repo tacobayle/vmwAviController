fc9d21f010af4c2524182c80abec1c8f8a583d31
Nicolas Bayle <nicolas.bayle.it@gmail.com>
2020-12-01 13:34:00 +0000

    20201201

--- ansible.tf
+++ ansible.tf
@@ -2,7 +2,15 @@ resource "null_resource" "ansible" {
   depends_on = [
     vsphere_virtual_machine.controller]
 
+
+  # ansible and pip package installation
   provisioner "local-exec" {
-    command = "sudo apt install python-pip ; pip install pyvmomi dnspython ansible==${var.ansible.version} avisdk==${var.ansible.avisdkVersion}; pip install --upgrade pip setuptools ; pip install --upgrade git+https://github.com/vmware/vsphere-automation-sdk-python.git ; sudo -u $USER ansible-galaxy install -f avinetworks.avisdk"
+    command = "sudo apt install python-pip ; pip install pyvmomi dnspython ansible==${var.ansible.version} avisdk==${var.ansible.avisdkVersion}; pip install --upgrade pip setuptools ; pip install --upgrade git+https://github.com/vmware/vsphere-automation-sdk-python.git"
   }
+
+  # Avi ansible role installation
+  provisioner "local-exec" {
+    command = "sudo -u $USER ansible-galaxy install -f avinetworks.avisdk"
+  }
+
 }
