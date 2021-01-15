7b7552445e50a6a07a6773db39768467ecea3a03
tacobayle <nicolas.bayle@gmail.com>
2021-01-15 13:50:57 +0000

    20210115

--- avi.tf
+++ avi.tf
@@ -36,17 +36,17 @@ resource "avi_cluster" "res_cluster" {
     ip {
       addr = "10.41.134.127"
       type ="v4"
-      },
+      }
     name = "10.41.134.128"
     ip {
       addr = "10.41.134.128"
       type ="v4"
-      },
+      }
     name = "10.41.134.129"
     ip {
       addr = "10.41.134.129"
       type ="v4"
-      },
+      }
     }
   }
   #nodes = [join(",", data.template_file.nodes.*.rendered)]
